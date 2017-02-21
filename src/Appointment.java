6256b960083d5ba42b53f4dd81599f0601838371
Mike <m3teitel@ryerson.ca>
2017-02-21 15:32:02 -0500

    test 2

--- src/Appointment.java
+++ src/Appointment.java
@@ -2,7 +2,8 @@
  * Created by Mike on 21-Feb-17.
  */
 public class Appointment {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello Part 2");
     }
 }
