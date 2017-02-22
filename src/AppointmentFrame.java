5ccf70e64c786dd92e5c7ed2ef04ba52ad3911fa
Mike <m3teitel@ryerson.ca>
2017-02-22 18:00:55 -0500

    did some work, basically done the appointment class, working on the frame class to do all the shit

--- src/AppointmentFrame.java
+++ src/AppointmentFrame.java
@@ -13,7 +13,7 @@ import java.util.GregorianCalendar;
  * Created by Mike on 2017-02-21.
  */
 public class AppointmentFrame extends JFrame {
-    private static int FRAME_WIDTH = 800;
+    private static int FRAME_WIDTH = 400;
     private static int FRAME_HEIGHT = 1000;
     private Calendar currentDate;
     private JLabel jl;
@@ -21,16 +21,18 @@ public class AppointmentFrame extends JFrame {
     private ArrayList<Appointment> appointments;
     private JTextArea ta;
     private JScrollBar sb;
-    private JPanel cp, dp, ap, desp;
+    private JPanel cp, dp, ap, desp, but, da;
     private JButton next, previous, show, create, cancel;
     private JTextField d, m, y, h, min;
 
     public AppointmentFrame(){
         setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        this.setLayout(new BorderLayout());
         appointments = new ArrayList<Appointment>();
         createDate();
         createTextArea();
         createControlPanel();
+        add(cp);
 
     }
     private void createDate(){
@@ -38,9 +40,11 @@ public class AppointmentFrame extends JFrame {
         sdf = new SimpleDateFormat("EEEE MMMM dd, yyyy");
         jl = new JLabel(sdf.format(currentDate.getTime()));
         add(jl, BorderLayout.NORTH);
+
     }
     private void createTextArea(){
         ta = new JTextArea();
+       // ta.setSize(700,600);
         sb = new JScrollBar();
         ta.add(sb);
         for(int i = 0; i > appointments.size(); i++){
@@ -51,23 +55,48 @@ public class AppointmentFrame extends JFrame {
     }
     private void createControlPanel(){
         cp = new JPanel();
+        cp.setLayout(new GridLayout(2,1));
         createDatePanel();
+        cp.add(dp);
 
 
     }
     private void createDatePanel(){
         dp = new JPanel();
+        dp.setLayout(new GridLayout(2,1));
         dp.setBorder(new TitledBorder(new EtchedBorder(), "Date"));
+        but = new JPanel();
+        da = new JPanel();
         next = new JButton(">");
+        next.setSize(500,300);
         previous = new JButton("<");
-        d = new JTextField("Day");
-        m = new JTextField("Month");
-        y = new JTextField("Year");
+        previous.setSize(500,300);
+        ActionListener pl = new PreviousListener();
+        previous.addActionListener(pl);
+        ActionListener nl = new NextListener();
+        next.addActionListener(nl);
+        d = new JTextField();
+        m = new JTextField();
+        y = new JTextField();
+        but.add(previous);
+        but.add(next);
+        da.add(d);
+        da.add(m);
+        da.add(y);
+        dp.add(but, 0);
+        dp.add(da, 1);
+
     }
     class PreviousListener implements ActionListener{
         public void actionPerformed(ActionEvent ae){
             currentDate.add(Calendar.DAY_OF_MONTH, -1);
-            jl.setText(sdf.format(currentDate));
+            jl.setText(sdf.format(currentDate.getTime()));
+        }
+    }
+    class NextListener implements ActionListener{
+        public void actionPerformed(ActionEvent ae){
+            currentDate.add(Calendar.DAY_OF_MONTH, 1);
+            jl.setText(sdf.format(currentDate.getTime()));
         }
     }
 }
